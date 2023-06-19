97973e888fc4116b12277a94292d56218983b353
Shivam <136954477+ShivamWebKids@users.noreply.github.com>
2023-06-20 00:23:58 +0530

    My Project

---
+++ java.js
@@ -0,0 +1,85 @@
+// let loGedIn =0;
+
+//  let shim = loGedIn == 0 ? 'Login' : 'Logout';
+
+//  alert (shim);
+
+
+//  
+
+// let user;
+
+//  user = "shivamVishwakarma";
+
+// alert ( user ?? 'Guest User');
+
+
+
+// if input = 1, 'y' , 'yes' = contnue;
+// if inout = 0,  'n',  'no' = end ; 
+
+/////////   //////// // this from here//////
+
+// let input = "yes";
+
+// if (input = 1) {
+//    document.write("continue.....");
+// } 
+// else if(input === 'y') {
+//     document.write("continue.....");
+// } 
+// else if (input === 'yes'){
+//     document.write("continue.....");
+// }
+// else if(input === 0) {
+//     document.write("END.....");
+// }
+// else if (input === 'n'){
+//     document.write("END.....");   
+// }
+// else if (input === 'no'){
+//     document.write("END.....");
+    
+// } 
+// else{
+ 
+//  alert (" Error");
+    
+// }
+   ////////////TO////////HE//////RE//////
+
+
+   // let counter = 1;
+   //  let sum = 0;
+   // while (counter<=100){
+      
+   //    if (counter % 2 == 0) {
+   //      sum = sum + counter
+   //    }
+     
+   //  counter++;
+   // }
+   //  document.write(sum);
+
+
+ // let name = prompt ('Enter Your Name', "shivam");
+
+ // if (name != null) {
+ //    document.write(`Your name is ${name}`);
+ // }
+ // else{
+ //  document.write("You Not enter Your name");
+ // }
+
+ // let sm= "shivam \\ vishwakarma";
+
+ // console.log(sm.length);
+
+let str = "This is My javascript Practic code and I want became a Developer";
+
+// let substring = str.substr(11, 19);
+
+  let substring = str.indexOf('became');
+   // let substring = str.toLowerCase();
+   let substring = str.toUpperCase();
+ document.write(substring);
